63388d27a46f02d576a56525d128ae44b4bd23b1
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-23 20:42:55 -0700

    added package.json and updated the app component.

--- Avengers/App.js
+++ Avengers/App.js
@@ -2,4 +2,23 @@ import React from "react";
 import { Route } from "react-router-dom";
 import AvengersList from "./Components/AvengersList";
 import Home from "./Components/Home";
-import "./aventers.css";
+import "./avengers.css";
+
+const App = () => {
+    const [savedList, setSavedList] = useState([]);
+
+    const addToSavedList = movie => {
+        setSavedList([...savedList, movie]);
+    };
+
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/avengers" component={AvengerList} />
+            </Switch>
+        </div>
+    );
+};
+
+export default App;
